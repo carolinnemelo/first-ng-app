a37e2033382a48bc2fe2360cca064480725b47af
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-04-30 11:21:27 +0200

    add header and home components with templates and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   template: `
-    <h1>Welcome to {{title}}!</h1>
+    <app-header />
+    <app-home />
 
-    <router-outlet />
   `,
   styles: [],
 })
