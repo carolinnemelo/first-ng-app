f41a9195d660a44d5101559d52c21235d6136d6d
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-05-02 14:12:16 +0200

    add highlight-completed-todo directive with input and effect for dynamic styling

--- src/app/directives/highlight-completed-todo.directive.ts
+++ src/app/directives/highlight-completed-todo.directive.ts
@@ -1,10 +1,21 @@
-import { Directive } from '@angular/core';
+import { Directive, input, effect, inject } from '@angular/core';
 
 @Directive({
-  selector: '[appHighlightCompletedTodo]'
+  selector: '[appHighlightCompletedTodo]',
 })
 export class HighlightCompletedTodoDirective {
+  isCompleted = input(false);
+  element = inject(ElementRef);
+  constructor() {}
 
-  constructor() { }
-
-}
+  stylesEffect = effect(() => {
+    if (!this.isCompleted()) {
+      this.element.nativeElement.style.textDecoration = 'line-through';
+      this.element.nativeElement.style.color = '#6c757d';
+      this.element.nativeElement.style.backgroundColor = '#d3f9d8';
+    }
+    this.element.nativeElement.style.textDecoration = 'none';
+    this.element.nativeElement.style.color = '#000';
+    this.element.nativeElement.style.backgroundColor = '#fff';
+  });
+} 
