a46ffe97e21ab8b4b2f03fd22effc93db2b3e3ce
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-05-05 10:52:32 +0200

    fix: correct logic in highlight-completed-todo directive for styling based on completion state

--- src/app/directives/highlight-completed-todo.directive.ts
+++ src/app/directives/highlight-completed-todo.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, input, effect, inject } from '@angular/core';
+import { Directive, input, effect, inject, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightCompletedTodo]',
@@ -6,16 +6,16 @@ import { Directive, input, effect, inject } from '@angular/core';
 export class HighlightCompletedTodoDirective {
   isCompleted = input(false);
   element = inject(ElementRef);
-  constructor() {}
 
   stylesEffect = effect(() => {
-    if (!this.isCompleted()) {
+    if (this.isCompleted()) {
       this.element.nativeElement.style.textDecoration = 'line-through';
       this.element.nativeElement.style.color = '#6c757d';
       this.element.nativeElement.style.backgroundColor = '#d3f9d8';
+    } else {
+      this.element.nativeElement.style.textDecoration = 'none';
+      this.element.nativeElement.style.color = '#000';
+      this.element.nativeElement.style.backgroundColor = '#fff';
     }
-    this.element.nativeElement.style.textDecoration = 'none';
-    this.element.nativeElement.style.color = '#000';
-    this.element.nativeElement.style.backgroundColor = '#fff';
   });
-} 
+}
