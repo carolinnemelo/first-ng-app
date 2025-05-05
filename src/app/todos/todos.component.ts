e16ff68348569be1474a4f06f7961a70a5281e21
Carolinne Melo <45534093+carolinnemelo@users.noreply.github.com>
2025-05-05 11:11:59 +0200

    feat: add FilterTodosPipe and integrate filtering functionality in todos component

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,22 +2,26 @@ import { Component, inject, OnInit, signal, Signal } from '@angular/core';
 import { TodosService } from '../services/todos.service';
 import { Todo } from '../model/todo.type';
 import { TodoItemComponent } from '../components/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 
 @Component({
   selector: 'app-todos',
-  imports: [TodoItemComponent],
+  imports: [TodoItemComponent, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
 export class TodosComponent implements OnInit {
   todosService = inject(TodosService);
   todoItems = signal<Todo[]>([]);
+  searchTerm = signal('');
 
   ngOnInit(): void {
     const todos = this.todosService.getTodosFromApi().subscribe((todos) => {
       this.todoItems.set(todos);
     });
   }
+
 
   updateTodoItem(todoItem: Todo) {
     this.todoItems.update((todos) => {
